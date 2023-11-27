d5f7d82274a4dd2e71b054a8ce5feeb05d050187
Kristian Lalov <lalov.kr@gmail.com>
2023-11-27 16:12:31 +0200

    add back button to folders page

--- src/pages/Folders.js
+++ src/pages/Folders.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-const Folders = () => {
+
+const Folders = props => {
+  const navigateHome = () => {
+    props.navigation.navigate('Home');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>This is the folders page</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Folders</Text>
+      <View style={styles.buttons_container}>
+        <TouchableOpacity onPress={navigateHome} style={styles.back_button}>
+          <Text style={styles.back_button}>Back</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {},
+  title: {
+    fontSize: 40,
+    margin: 25,
+    fontWeight: 'bold',
+  },
+  back_button: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  buttons_container: {
+    position: 'absolute',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 25,
+    borderColor: 'black',
+    borderWidth: 1,
+    bottom: 10,
+  },
 });
 
 export default Folders;
